8a7cff3df382ea6a4ed9da121552e379e9306a5d
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-02-21 18:59:39 +0000

    html have been settled and js has been startedt

---
+++ script.js
@@ -0,0 +1,9 @@
+/// add item adinda bir function ekledim
+document.querySelector("button").setAttribute('onclick','addItem()')
+////unordered list ekledim
+const ul=document.createElement('ul')
+//unordered listi divden sonraya ekledim
+document.querySelector('div').after(ul)
+///Inputu variable'a atadim
+const input= document.querySelector('input')
+//Listi olsutup havada ututuom
