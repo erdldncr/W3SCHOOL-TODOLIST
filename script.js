b7c37da22beacb784fcc7fb3730afb6ca8ad6930
Erdal dıncer <erdaldincer@Erdals-MacBook-Pro.local>
2021-02-22 06:34:28 +0000

    css has been adjusted

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ document.querySelector('div').after(ul)
 ///Inputu variable'a atadim
 const input= document.querySelector('input')
 //Listi olsutup havada ututuom
+
+function addItem(){
+ if(input.value){
+    const li=document.createElement('li')
+    li.innerHTML=input.value
+    li.setAttribute('onclick','liClick(this)')
+    ul.appendChild(li)
+ }else{
+     alert('please write something')
+ }
+}
+function liClick(element){
+  element.innerHTML= `<strike> ${element.innerHTML}</strike>`  
+  element.prepend(document.createTextNode('✅'))
+element.style.color="#FFF"
+element.style.backgroundColor='rgb(101,136,128)'
+}
